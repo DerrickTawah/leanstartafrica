bf63665d3818c0398190d2a98f1bb5dec0a3222a
DerrickTawah <atuakom@gmail.com>
2026-08-23 21:46:32 +0200

    changed button text on hali.africa

--- components/VentureCard.tsx
+++ components/VentureCard.tsx
@@ -57,7 +57,7 @@ const VentureCard: React.FC<VentureCardProps> = ({ venture }) => {
               if (venture.url === '#') e.preventDefault();
             }}
           >
-            {isLive ? 'Visit Website' : isRedirecting ? 'Back to Start' : 'Coming Soon'}
+            {isLive ? 'Visit Website' : isRedirecting ? 'Visit' : 'Coming Soon'}
             {(isLive || isRedirecting) && (
               <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
